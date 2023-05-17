e69db67f20dfc61f1f6e2a3ef55da32b445b164c
gryaznykh-ivan <gryaznykh.ivan@gmail.com>
2023-05-17 17:16:32 +0300

    parser update

--- parser/server/src/utils/request/request.service.ts
+++ parser/server/src/utils/request/request.service.ts
@@ -10,6 +10,10 @@ export class RequestService {
     constructor(
         private proxy: ProxyService
     ) { }
+    
+    private getRandomBetween(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min) + min)
+    }
 
     async getFetcher() {
         const proxyUrl = await this.proxy.createOrUpdateProxy()
@@ -20,6 +24,11 @@ export class RequestService {
         headers.append('accept', '*/*')
         headers.append('accept-language', 'en-US,en;q=0.9')
         headers.append('user-agent', userAgent.toString())
+
+        // for perimeterX
+        const r3_16 = this.getRandomBetween(0, 4095).toString(16).padEnd(3, "0")
+        const r4_16 = this.getRandomBetween(0, 65535).toString(16).padEnd(4, "0")
+        headers.append('cookie', `_pxvid=${r4_16}${r4_16}-${r4_16}-11ed-8${r3_16}-${r4_16}${r4_16}${r4_16}`)
 
         return this.fetch({
             method: 'GET',
